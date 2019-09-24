6ee8e06dc476866b9f70d3a80832f0a340825f91
eldenhowes <williameldenhowes@gmail.com>
2019-09-24 10:17:57 +0100

    Handles larger arrays with mixed numbers and letters

--- lib/shuffle_algorithm.rb
+++ lib/shuffle_algorithm.rb
@@ -1,10 +1,9 @@
 class Array
 
   def shuffle_algorithm
-    if self.length == 2
+    if self.length > 1
       return self.permutation.to_a[rand(0..self.length)]
     end
     self
   end
-
 end
--- spec/shuffle_algorithm_spec.rb
+++ spec/shuffle_algorithm_spec.rb
@@ -5,14 +5,19 @@ describe '#shuffle_algorithm' do
     expect([0].shuffle_algorithm).to eq([0])
   end
 
-  it 'returns a shuffled array for two items' do
+  before(:each) do
     allow_any_instance_of(Object).to receive(:rand).and_return(1)
+  end
+
+  it 'returns a shuffled array for two items' do
     expect([0,1].shuffle_algorithm).to eq([1,0])
   end
 
   it 'returns a shuffled array for two letters' do
-    allow_any_instance_of(Object).to receive(:rand).and_return(1)
     expect(['a','b'].shuffle_algorithm).to eq(['b','a'])
+  end
 
+  it 'returns a shuffled array for given a larger array of numbers and letters' do
+    expect([1, 'a', 2, 'b', 3, 'c'].shuffle_algorithm).to eq([1, "a", 2, "b", "c", 3])
   end
 end
